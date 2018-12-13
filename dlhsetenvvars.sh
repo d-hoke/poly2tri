86bb5ed70b66c2f8f9de6f7c30487262a0a3c1a1
dhoke <dhoke@flyinside-fsx.com>
2018-12-13 11:04:34 -0500

    not sure what mishmash this is, wanted hexlord's (Github) fork changes, but not sure I've got them here.... maybe throw this away...

---
+++ dlhsetenvvars.sh
@@ -0,0 +1,4 @@
+export CPATH=/f/dev/opengllearn/github.glfw.glfw-legacy.git/include
+# export LIBPATH=/f/dev/opengllearn/github.glfw.glfw-legacy.git/lib/win32
+export LIBRARY_PATH=/f/dev/opengllearn/github.glfw.glfw-legacy.git/lib/win32
+export PATH=$PATH:/f/dev/opengllearn/github.glfw.glfw-legacy.git/win32
